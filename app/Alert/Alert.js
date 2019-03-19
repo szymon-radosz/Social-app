f36c3d5d504e56d70c39c686b6a4cc5a3b3c6016
RadoszSzymon <radoszszymon@gmail.com>
2019-03-19 17:26:07 +0100

    close alert

--- app/Alert/Alert.js
+++ app/Alert/Alert.js
@@ -1,28 +1,59 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Button, Text, View } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  Button,
+  Text,
+  View,
+  TouchableHighlight
+} from "react-native";
 import styles from "./style";
 
 export default class Alert extends Component {
+  static getDerivedStateFromProps(props, current_state) {
+    if (current_state.alertMessage !== props.alertMessage) {
+      console.log("derivered");
+      return {
+        showAlert: true,
+        alertMessage: props.alertMessage
+      };
+    }
+    return null;
+  }
+
   constructor(props) {
     super(props);
-    this.state = { showAlert: false };
+    this.state = { showAlert: true, alertMessage: "" };
+
+    this.closeAlert = this.closeAlert.bind(this);
+  }
+
+  closeAlert() {
+    this.setState({ showAlert: false });
   }
 
   render() {
-    return (
-      <View style={styles.alertContainer}>
-        <Text
-          style={
-            this.props.alertType == "success"
-              ? styles.successContainer
-              : this.props.alertType == "danger"
-              ? styles.dangerContainer
-              : null
-          }
-        >
-          {this.props.alertMessage}
-        </Text>
-      </View>
-    );
+    if (this.state.showAlert) {
+      return (
+        <View style={styles.alertContainer}>
+          <Text
+            style={
+              this.props.alertType == "success"
+                ? styles.successContainer
+                : this.props.alertType == "danger"
+                ? styles.dangerContainer
+                : null
+            }
+          >
+            {this.state.alertMessage}
+          </Text>
+          <TouchableHighlight style={{ position: "absolute", right: 10 }}>
+            <Button title="X" color="#fff" onPress={() => this.closeAlert()} />
+          </TouchableHighlight>
+        </View>
+      );
+    } else {
+      return <View />;
+    }
   }
 }
