3378b2153ce60cc6c0711a3297fe41fbc38b0cb2
RadoszSzymon <radoszszymon@gmail.com>
2019-04-19 20:02:11 +0200

    sort forum posts  by category

--- app/LoggedInScreens/Forum/Forum.tsx
+++ app/LoggedInScreens/Forum/Forum.tsx
@@ -11,6 +11,7 @@ import axios from "axios";
 import SinglePostOnList from "./utils/SinglePostOnList";
 import PostDetails from "./utils/PostDetails";
 import SavePost from "./utils/SavePost";
+import SortByCategory from "./utils/SortByCategory";
 import styles from "./style";
 
 interface ForumProps {
@@ -20,9 +21,11 @@ interface ForumProps {
 
 interface ForumState {
   showPostDetails: boolean;
+  showSortByCategory: boolean;
   showSavePost: boolean;
   showPostDetailsId: number;
   postList: any;
+  showSortByCategoryId: number;
 }
 export default class Forum extends Component<ForumProps, ForumState> {
   constructor(props: ForumProps) {
@@ -30,7 +33,9 @@ export default class Forum extends Component<ForumProps, ForumState> {
     this.state = {
       showPostDetails: false,
       showSavePost: false,
+      showSortByCategory: false,
       showPostDetailsId: 0,
+      showSortByCategoryId: 0,
       postList: []
     };
 
@@ -38,18 +43,61 @@ export default class Forum extends Component<ForumProps, ForumState> {
     this.setShowPostDetails = this.setShowPostDetails.bind(this);
     this.savePost = this.savePost.bind(this);
     this.setShowSavePost = this.setShowSavePost.bind(this);
+    this.setShowSortByCategory = this.setShowSortByCategory.bind(this);
+    this.getPostByCategoryId = this.getPostByCategoryId.bind(this);
   }
 
   setShowPostDetails = (): void => {
     this.setState(prevState => ({
       showPostDetails: !prevState.showPostDetails
     }));
+    if (this.state.showSortByCategoryId === 0) {
+      this.getPosts();
+    } else {
+      this.getPostByCategoryId(this.state.showSortByCategoryId, false);
+    }
   };
 
   setShowSavePost = (): void => {
     this.setState(prevState => ({
       showSavePost: !prevState.showSavePost
     }));
+  };
+
+  setShowSortByCategory = (): void => {
+    this.setState(prevState => ({
+      showSortByCategory: !prevState.showSortByCategory
+    }));
+  };
+
+  getPostByCategoryId = (categoryId: number, closeModal: boolean): void => {
+    let API_URL = this.props.API_URL;
+
+    let that = this;
+
+    axios
+      .post(API_URL + "/api/getPostByCategoryId", {
+        categoryId: categoryId
+      })
+      .then(function(response) {
+        if (response.data.status === "OK") {
+          //console.log(["getAuctionProducts", response]);
+          that.setState({ postList: [] });
+          that.setState({
+            showSortByCategoryId: categoryId,
+            postList: response.data.result
+          });
+
+          if (closeModal) {
+            that.setShowSortByCategory();
+          }
+
+          console.log(response.data);
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   };
 
   getPosts = (): void => {
@@ -108,7 +156,7 @@ export default class Forum extends Component<ForumProps, ForumState> {
         .then(function(response) {
           if (response.data.status === "OK") {
             console.log(["savePost", response]);
-            that.setState({ showSavePost: false });
+            that.setState({ showSavePost: false, showSortByCategoryId: 0 });
             that.getPosts();
           } else {
             console.log(response.data.result);
@@ -129,11 +177,21 @@ export default class Forum extends Component<ForumProps, ForumState> {
   render() {
     return (
       <View style={styles.container}>
-        <Text>E-mamy.pl Forum</Text>
+        <TouchableHighlight>
+          <Button
+            title="Sortowanie"
+            onPress={(): void => {
+              this.setShowSortByCategory();
+            }}
+            color="#000"
+          />
+        </TouchableHighlight>
+        <Text>Posty</Text>
 
         {this.state.showPostDetails &&
           this.state.showPostDetailsId &&
-          !this.state.showSavePost && (
+          !this.state.showSavePost &&
+          !this.state.showSortByCategory && (
             <PostDetails
               postDetailsId={this.state.showPostDetailsId}
               API_URL={this.props.API_URL}
@@ -142,14 +200,28 @@ export default class Forum extends Component<ForumProps, ForumState> {
             />
           )}
 
-        {!this.state.showPostDetails && this.state.showSavePost && (
-          <SavePost
-            API_URL={this.props.API_URL}
-            user={this.props.user}
-            savePost={this.savePost}
-            setShowSavePost={this.setShowSavePost}
-          />
-        )}
+        {!this.state.showPostDetails &&
+          !this.state.showSortByCategory &&
+          this.state.showSavePost && (
+            <SavePost
+              API_URL={this.props.API_URL}
+              user={this.props.user}
+              savePost={this.savePost}
+              setShowSavePost={this.setShowSavePost}
+            />
+          )}
+
+        {!this.state.showPostDetails &&
+          !this.state.showSavePost &&
+          this.state.showSortByCategory && (
+            <SortByCategory
+              API_URL={this.props.API_URL}
+              user={this.props.user}
+              getPostByCategoryId={this.getPostByCategoryId}
+              showSortByCategoryId={this.state.showSortByCategoryId}
+              setShowSortByCategory={this.setShowSortByCategory}
+            />
+          )}
 
         <ScrollView>
           {this.state.postList &&
--- app/LoggedInScreens/Forum/utils/SortByCategory.tsx
+++ app/LoggedInScreens/Forum/utils/SortByCategory.tsx
@@ -15,32 +15,29 @@ import { v4 as uuid } from "uuid";
 import styles from "./../style";
 import axios from "axios";
 
-interface SavePostState {
-  title: string;
-  description: string;
-  categoryId: number;
-  categories: any;
+interface SortByCategoryState {
+  categories: [];
 }
 
-interface SavePostProps {
+interface SortByCategoryProps {
   API_URL: string;
   user: any;
-  savePost: any;
-  setShowSavePost: any;
+  setShowSortByCategory: any;
+  getPostByCategoryId: any;
+  showSortByCategoryId: number;
 }
 
-export default class SavePost extends Component<SavePostProps, SavePostState> {
-  constructor(props: SavePostProps) {
+export default class SortByCategory extends Component<
+  SortByCategoryProps,
+  SortByCategoryState
+> {
+  constructor(props: SortByCategoryProps) {
     super(props);
     this.state = {
-      title: "",
-      description: "",
-      categories: [],
-      categoryId: 0
+      categories: []
     };
 
     this.getCategories = this.getCategories.bind(this);
-    this.selectCategoryId = this.selectCategoryId.bind(this);
   }
 
   getCategories = (): void => {
@@ -65,11 +62,6 @@ export default class SavePost extends Component<SavePostProps, SavePostState> {
     }
   };
 
-  selectCategoryId = (categoryId: number): void => {
-    this.setState({ categoryId: categoryId });
-    console.log(categoryId);
-  };
-
   componentDidMount = (): void => {
     this.getCategories();
   };
@@ -85,55 +77,28 @@ export default class SavePost extends Component<SavePostProps, SavePostState> {
               <Button
                 title="X"
                 color="#000"
-                onPress={() => this.props.setShowSavePost()}
+                onPress={() => this.props.setShowSortByCategory()}
               />
             </TouchableHighlight>
 
             <ScrollView style={styles.postDetailsContainer}>
-              <TextInput
-                multiline={false}
-                onChangeText={title => this.setState({ title })}
-                value={this.state.title}
-                placeholder="Temat"
-                placeholderTextColor="#333"
-              />
-
-              <TextInput
-                multiline={false}
-                onChangeText={description => this.setState({ description })}
-                value={this.state.description}
-                placeholder="Treść postu"
-                placeholderTextColor="#333"
-              />
-
               {categories.map((category: any, i: number) => {
                 return (
                   <TouchableHighlight>
                     <Button
                       title={category.name}
-                      onPress={() => this.selectCategoryId(category.id)}
+                      onPress={() =>
+                        this.props.getPostByCategoryId(category.id, true)
+                      }
                       color={
-                        category.id === this.state.categoryId ? "blue" : "#000"
+                        category.id === this.props.showSortByCategoryId
+                          ? "blue"
+                          : "#000"
                       }
                     />
                   </TouchableHighlight>
                 );
               })}
-
-              <TouchableHighlight>
-                <Button
-                  title="Dodaj"
-                  onPress={() =>
-                    this.props.savePost(
-                      this.state.title,
-                      this.state.description,
-                      this.props.user.id,
-                      this.state.categoryId
-                    )
-                  }
-                  color="#000"
-                />
-              </TouchableHighlight>
             </ScrollView>
           </View>
         </View>
